fb82f877668ccf6dce4cc73063baee00f103eb33
Yuka-Makino <ec2-user@ip-172-31-28-11.ap-northeast-1.compute.internal>
2023-06-24 12:07:01 +0000

    16

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,16 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to books_path(book.id)
+    if book.save 
+      flash[:notice] = "Books was succesfully created."
+      redirect_to book_path(book.id)
+    else render :new
+      end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -24,12 +28,14 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to books_path(book.id) 
+    flash[:notice] = "Books was succesfully created."
+    redirect_to book_path(book.id) 
   end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Books was succesfully created."
     redirect_to '/books'
   end
   
