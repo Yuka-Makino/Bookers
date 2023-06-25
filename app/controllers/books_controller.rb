747c5f6713b87a60a07dc1c5e44ad03f6e2fafea
Yuka-Makino <ec2-user@ip-172-31-28-11.ap-northeast-1.compute.internal>
2023-06-25 09:44:08 +0000

    Test END

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     if @book.save 
       redirect_to book_path(@book.id)
-      flash[:notice] = "Books was succesfully created."
+      flash[:notice] = "Books was successfully created."
     else 
       @books = Book.all
       render :index
@@ -31,7 +31,7 @@ class BooksController < ApplicationController
     @book.update(book_params)
     if @book.save 
       redirect_to book_path(@book.id)
-      flash[:notice] = "Books was succesfully created."
+      flash[:notice] = "Books was successfully created."
     else 
       @books = Book.all
       render :edit
